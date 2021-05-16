43d30fac5b90d6042cb8437581fb28740c5dba3e
Nicole <nicole929chan@gmail.com>
2021-05-16 16:52:52 +0800

    fix, items migration

--- database/migrations/2021_05_16_070659_create_items_table.php
+++ database/migrations/2021_05_16_070659_create_items_table.php
@@ -16,12 +16,13 @@ class CreateItemsTable extends Migration
         Schema::create('items', function (Blueprint $table) {
             $table->id();
             $table->string('category_slug')->comment('物品類型代碼');
-            $table->string('box_slug')->comment('箱子代碼');;
-            $table->text('description')->nullable()->comment('拾獲說明');
+            $table->string('box_slug')->comment('箱子代碼');
+            $table->string('place')->comment('拾獲地點');
+            $string->string('image01')->nullable('物品照片');
             $table->string('contact_name')->nullable()->comment('拾獲聯絡人');
             $table->string('contact_email')->nullable()->comment('拾獲聯絡人郵件');
             $table->string('contact_phone_number')->nullable()->comment('拾獲聯絡人手機');
-            $table->datetime('finished_at')->nullable()->comment('結案時間');
+            $table->datetime('finished_at')->nullable()->comment('領取時間');
             $table->timestamps();
         });
     }
